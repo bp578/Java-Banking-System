c96fe02f48a8e9838e20fa1da75d0905a3c12f1e
bp578 <bp578@drexel.edu>
2023-05-14 22:51:04 -0400

    Added test cases for the fourth and fifth argument.

--- src/main/java/CreateCommandValidator.java
+++ src/main/java/CreateCommandValidator.java
@@ -2,43 +2,49 @@ public class CreateCommandValidator {
 	private String[] command;
 	private String action; // arg 1
 	private String accountType; // arg 2
-	private String accountId;
+	private String accountId; // arg 3
+	private String apr; // arg 4
+	private String balance; // arg 5 (cd only)
 
 	// All tests on each part of the command must pass for the whole command to pass
 	public boolean validate(String commandStr) {
 		this.command = commandStr.split(" ");
 
-		// Check if the argument count is valid before continuing
 		if (command.length == 4) {
 			this.action = command[0];
 			this.accountType = command[1];
 			this.accountId = command[2];
+			this.apr = command[3];
+			return actionIsValid(action) && accountTypeIsValid(accountType) && accountIdIsValid(accountId)
+					&& APRisValid(apr);
+		} else if (command.length == 5) {
+			this.action = command[0];
+			this.accountType = command[1];
+			this.accountId = command[2];
+			this.apr = command[3];
+			this.balance = command[4];
+			return actionIsValid(action) && accountTypeIsValid(accountType) && accountIdIsValid(accountId)
+					&& APRisValid(apr) && balanceIsValid(balance);
 		} else {
 			return false;
 		}
 
-		if (actionIsValid(action) && accountTypeIsValid(accountType) && accountIdIsValid(accountId)) {
-			return true;
-		} else {
-			return false;
-		}
 	}
 
 	private boolean actionIsValid(String action) {
-		if (action.equalsIgnoreCase("create")) {
-			return true;
-		} else {
-			return false;
-		}
+		return action.equalsIgnoreCase("create");
 	}
 
 	private boolean accountTypeIsValid(String accountType) {
-		if (accountType.equalsIgnoreCase("savings") || accountType.equalsIgnoreCase("checking")
-				|| accountType.equalsIgnoreCase("cd")) {
+		if ((accountType.equalsIgnoreCase("savings") || accountType.equalsIgnoreCase("checking"))
+				&& command.length == 4) {
+			return true;
+		} else if (accountType.equalsIgnoreCase("cd") && command.length == 5) {
 			return true;
 		} else {
 			return false;
 		}
+
 	}
 
 	private boolean accountIdIsValid(String accountId) {
@@ -54,6 +60,24 @@ public class CreateCommandValidator {
 		}
 
 		return true;
+	}
+
+	private boolean APRisValid(String aprStr) {
+		try {
+			Double APR = Double.parseDouble(aprStr);
+			return APR >= 0 && APR <= 10;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+	private boolean balanceIsValid(String balanceStr) {
+		try {
+			double balance = Double.parseDouble(balanceStr);
+			return balance > 0 && balance <= 10000;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 
 }
--- src/test/java/CreateCommandValidatorTest.java
+++ src/test/java/CreateCommandValidatorTest.java
@@ -69,7 +69,7 @@ public class CreateCommandValidatorTest {
 	public void second_argument_must_be_an_account_type() {
 		String savingsCommand = "create savings 12345678 0.1";
 		String checkingCommand = "create checking 12345678 0.1";
-		String cdCommand = "create cd 12345678 0.1";
+		String cdCommand = "create cd 12345678 0.1 500";
 
 		assertTrue(createCommandValidator.validate(savingsCommand));
 		assertTrue(createCommandValidator.validate(checkingCommand));
@@ -112,7 +112,7 @@ public class CreateCommandValidatorTest {
 	}
 
 	@Test
-	public void ID_must_contain_numbers_only() {
+	public void ID_must_be_a_number() {
 		String command = "create savings 12EA56T8 0.1";
 		boolean actual = createCommandValidator.validate(command);
 
@@ -130,6 +130,121 @@ public class CreateCommandValidatorTest {
 	@Test
 	public void ID_cannot_be_more_than_8_characters() {
 		String command = "create savings 123456789 0.1";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertFalse(actual);
+	}
+
+	// Tests dealing with the fourth argument (APR)
+	@Test
+	public void missing_APR_is_invalid() {
+		String command = "create savings 12345678";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertFalse(actual);
+	}
+
+	@Test
+	public void fourth_argument_must_be_APR() {
+		String command = "create savings 12345678 0.1";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertTrue(actual);
+	}
+
+	@Test
+	public void APR_must_be_a_number() {
+		String command = "create savings 12345678 abc";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertFalse(actual);
+	}
+
+	@Test
+	public void APR_can_be_int() {
+		String command = "create savings 12345678 5";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertTrue(actual);
+	}
+
+	@Test
+	public void APR_can_be_0() {
+		String command = "create savings 12345678 0";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertTrue(actual);
+	}
+
+	@Test
+	public void APR_cannot_be_less_than_0() {
+		String command = "create savings 12345678 -1";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertFalse(actual);
+	}
+
+	@Test
+	public void APR_cannot_be_greater_than_10() {
+		String command = "create savings 12345678 11";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertFalse(actual);
+	}
+
+	// Tests dealing with the fifth argument (balance) CD accounts only
+	@Test
+	public void missing_balance_is_invalid() {
+		String command = "create cd 12345678 1.2";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertFalse(actual);
+	}
+
+	@Test
+	public void adding_balance_to_cd_account_is_valid() {
+		String command = "create cd 12345678 1.2 500";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertTrue(actual);
+	}
+
+	@Test
+	public void balance_must_be_a_number() {
+		String command = "create cd 12345678 1.2 abc";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertFalse(actual);
+	}
+
+	@Test
+	public void adding_balance_to_savings_or_checking_account_is_invalid() {
+		String savingsCommand = "create savings 12345678 1.2 500";
+		String checkingCommand = "create checking 12345678 1.2 500";
+
+		assertFalse(createCommandValidator.validate(savingsCommand));
+		assertFalse(createCommandValidator.validate(checkingCommand));
+	}
+
+	@Test
+	public void balance_cannot_be_0() {
+		String command = "create cd 12345678 1.2 0";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertFalse(actual);
+	}
+
+	@Test
+	public void balance_cannot_be_negative() {
+		String command = "create cd 12345678 1.2 -1";
+		boolean actual = createCommandValidator.validate(command);
+
+		assertFalse(actual);
+	}
+
+	@Test
+	public void balance_cannot_be_greater_than_10000() {
+		String command = "create cd 12345678 1.2 10001";
 		boolean actual = createCommandValidator.validate(command);
 
 		assertFalse(actual);
