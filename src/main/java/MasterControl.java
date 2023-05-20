e5624a3a93df9c0cb4443e0d46d35a1e02738c63
bp578 <bp578@drexel.edu>
2023-05-20 03:33:56 -0400

    Finished work on MasterControl and testing for it

--- src/main/java/MasterControl.java
+++ src/main/java/MasterControl.java
@@ -1,2 +1,25 @@
+import java.util.List;
+
 public class MasterControl {
+	CommandValidator commandValidator;
+	CommandProcessor commandProcessor;
+	CommandStorage commandStorage;
+
+	public MasterControl(CommandValidator commandValidator, CommandProcessor commandProcessor,
+			CommandStorage commandStorage) {
+		this.commandValidator = commandValidator;
+		this.commandProcessor = commandProcessor;
+		this.commandStorage = commandStorage;
+	}
+
+	public List<String> start(List<String> input) {
+		for (String command : input) {
+			if (commandValidator.validate(command)) {
+				commandProcessor.run(command);
+			} else {
+				commandStorage.add(command);
+			}
+		}
+		return commandStorage.getCommands();
+	}
 }
